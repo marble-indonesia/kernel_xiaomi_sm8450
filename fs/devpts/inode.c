a96047d8cd69c4c8ddc9bea9b862103846072d91
Pzqqt <821026875@qq.com>
2025-01-22 09:49:50 +0800

    KernelSU: Allows manual hooking without using kprobes
    Some apps will detect that KernelSU uses kprobes and think that the device
    environment is abnormal. Therefore, to avoid being detected, it is best to
    follow the official documentation of KernelSU to manually hook, just like
    doing it on a traditional kernel.
    
    This commit allows us to manually hook while keeping `CONFIG_KPROBES` enabled.
    
    Notes:
    The kernel having kprobes support does not mean that the environment is
    abnormal, because GKI has kprobes support by default, and disabling kprobes
    support will make it impossible to start the device.
    
    Signed-off-by: Pzqqt <821026875@qq.com>
    
    # Conflicts:
    #	fs/devpts/inode.c
    #	fs/open.c

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -604,6 +604,10 @@ extern bool ksu_devpts_hook;
 extern int ksu_handle_devpts(struct inode*);
 #endif
 
+#ifdef CONFIG_KSU_MANUAL_HOOK
+extern int ksu_handle_devpts(struct inode*);
+#endif
+
 /**
  * devpts_get_priv -- get private data for a slave
  * @pts_inode: inode of the slave
@@ -612,6 +616,9 @@ extern int ksu_handle_devpts(struct inode*);
  */
 void *devpts_get_priv(struct dentry *dentry)
 {
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	ksu_handle_devpts(dentry->d_inode);
+#endif
 #if defined(CONFIG_KSU_SUSFS_SUS_SU)
 	if (likely(ksu_devpts_hook)) {
 		ksu_handle_devpts(dentry->d_inode);
