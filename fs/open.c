87248631451f7c69892a406982d48a96c8e77e7d
Pzqqt <821026875@qq.com>
2025-01-22 09:49:50 +0800

    kernel & KernelSU: Allows manual hooking without using kprobes
    Some apps will detect that KernelSU uses kprobes and think that the device
    environment is abnormal. Therefore, to avoid being detected, it is best to
    follow the official documentation of KernelSU to manually hook, just like
    doing it on a traditional kernel.
    
    This commit allows us to manually hook while keeping `CONFIG_KPROBES` enabled.
    
    Notes:
    The kernel having kprobes support does not mean that the environment is
    abnormal, because GKI has kprobes support by default, and disabling kprobes
    support will make it impossible to start the device.
    
    Change-Id: I36595e25c96044eba95109343d28f6889d3bc558
    Signed-off-by: Pzqqt <821026875@qq.com>

--- fs/open.c
+++ fs/open.c
@@ -324,6 +324,11 @@ int vfs_fallocate(struct file *file, int mode, loff_t offset, loff_t len)
 }
 EXPORT_SYMBOL_GPL(vfs_fallocate);
 
+#ifdef CONFIG_KSU_MANUAL_HOOK
+extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
+                                int *flags);
+#endif
+
 int ksys_fallocate(int fd, int mode, loff_t offset, loff_t len)
 {
 	struct fd f = fdget(fd);
@@ -422,6 +427,10 @@ static long do_faccessat(int dfd, const char __user *filename, int mode, int fla
 	if (likely(susfs_is_sus_su_hooks_enabled)) {
 		ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
 	}
+#endif
+
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
 #endif
 
 	if (mode & ~S_IRWXO)	/* where's F_OK, X_OK, W_OK, R_OK? */
