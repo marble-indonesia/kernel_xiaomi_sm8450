ca52adc500359006ea91ca27d61c63ac927b4e8e
bachnxuan <bachnxuan@gmail.com>
2025-06-10 14:29:04 +0700

    KernelSU: Implement scope-minimized manual hooks v1.4 This refactors the original KernelSU hooks, replacing deep kernel function hooks with more targeted, efficient alternatives. It backports changes from KernelSU PR #1657 and incorporates elements from PR #2084 (32-bit sucompat). The goal is to minimize the scope of kernel function interception while preserving full functionality.
    References:
    - KernelSU PR #1657, PR #2084
    - backslashxx/KernelSU#5
    - https://kernelsu.org/guide/how-to-integrate-for-non-gki.html
    
    Co-Authored-By: backslashxx <118538522+backslashxx@users.noreply.github.com>
    Signed-off-by: bachnxuan <bachnxuan@gmail.com>
    
    # Conflicts:
    #	drivers/input/input.c
    #	fs/open.c
    #	fs/read_write.c
    #	fs/stat.c

--- fs/read_write.c
+++ fs/read_write.c
@@ -639,6 +639,12 @@ ssize_t ksys_read(unsigned int fd, char __user *buf, size_t count)
 	return ret;
 }
 
+#ifdef CONFIG_KSU
+extern bool ksu_vfs_read_hook __read_mostly;
+extern int ksu_handle_sys_read(unsigned int fd, char __user **buf_ptr,
+			size_t *count_ptr);
+#endif
+
 #ifdef CONFIG_KSU_MANUAL_HOOK
 extern bool ksu_vfs_read_hook __read_mostly;
 extern int ksu_handle_sys_read(unsigned int fd, char __user **buf_ptr,
@@ -648,7 +654,11 @@ extern int ksu_handle_sys_read(unsigned int fd, char __user **buf_ptr,
 SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 {
 #ifdef CONFIG_KSU_MANUAL_HOOK
-	if (unlikely(ksu_vfs_read_hook)) 
+	if (unlikely(ksu_vfs_read_hook))
+		ksu_handle_sys_read(fd, &buf, &count);
+#endif
+#ifdef CONFIG_KSU
+	if (unlikely(ksu_vfs_read_hook))
 		ksu_handle_sys_read(fd, &buf, &count);
 #endif
 	return ksys_read(fd, buf, count);
