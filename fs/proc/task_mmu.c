f28b6bbed82968ddeebeba09978e58efeead08b2
pavelc4 <dimzzy23@gmail.com>
2025-07-31 22:26:55 +0700

    Revert "chore(kernel): update SUSFS patch to version 1.5.8"
    This reverts commit 1b8f2bcef7901a8a9f834020d2542e163496d5ae.

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -380,7 +380,8 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
             	}
         	}
     	}
-    	}
+	}
+
 	start = vma->vm_start;
 	end = vma->vm_end;
 	show_vma_header_prefix(m, start, end, flags, pgoff, dev, ino);
