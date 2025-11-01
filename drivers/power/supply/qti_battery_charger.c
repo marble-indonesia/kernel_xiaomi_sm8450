ad9ace3f6df5469b1f51b5ef4cb93b428fba0ad6
Fiqri Ardyansyah <fiqri191002@gmail.com>
2025-11-01 21:32:13 +0700

    power: supply: qti_battery_charger: Add batt_sn, max_life_{vol,temp} and over_vol_duration nodes
    Signed-off-by: Maulana Riady <maulriaddy@gmail.com>

--- drivers/power/supply/qti_battery_charger.c
+++ drivers/power/supply/qti_battery_charger.c
@@ -1260,8 +1260,9 @@ static void handle_message(struct battery_chg_dev *bcdev, void *data,
 				memcpy(bcdev->wls_debug_data, chg_debug_data->data, CHG_DEBUG_DATA_LEN);
 				ack_set = true;
 			} else if (chg_debug_data->type == CHG_BATT_SN_CODE) {
-				memset(bcdev->batt_sn_data, '\0', CHG_DEBUG_DATA_LEN);
-				memcpy(bcdev->batt_sn_data, chg_debug_data->data, CHG_DEBUG_DATA_LEN);
+				memcpy(bcdev->batt_sn_data,
+				       chg_debug_data->data,
+				       CHG_DEBUG_DATA_LEN);
 				ack_set = true;
 			}
 			break;
