06169c27643c8ffdd76117d08db8c6b097662f1c
Maulana Riaddy <maulriaddy@gmail.com>
2026-04-13 06:06:05 +0000

    gki_defconfig: Lite version name
    Signed-off-by: Maulana Riaddy <maulriaddy@gmail.com>

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ LOG_FILE="log.txt"
 
 KP_ROOT="$(realpath ../..)"
 SRC_ROOT="$HOME/pa"
-TC_DIR="$KP_ROOT/prebuilts-master/clang/host/linux-x86/greenforce-clang"
+TC_DIR="$KP_ROOT/prebuilts-master/clang/host/linux-x86/llvm-22"
 PREBUILTS_DIR="$KP_ROOT/prebuilts/kernel-build-tools/linux-x86"
 DO_CLEAN=false
 NO_LTO=false
@@ -128,7 +128,7 @@ echo -e "Generating config...\n"
 mkdir -p out
 m $DEFCONFIG
 m ./scripts/kconfig/merge_config.sh $DEFCONFIGS vendor/${TARGET}_GKI.config
-scripts/config --file out/.config --set-str LOCALVERSION "-ZettaAshikaga."
+scripts/config --file out/.config --set-str LOCALVERSION "-ZettaKernel-Lite."
 
 if $NO_LTO; then
     scripts/config --file out/.config -d LTO_CLANG_FULL -e LTO_NONE --set-str LOCALVERSION "-Zetta.Ocen-Kernel-NoLTO"
