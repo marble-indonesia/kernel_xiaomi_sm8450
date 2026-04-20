c3241070f3d97ea0e201ec37f2f4feb6667b8e91
Templar Dev <wilfridus.it@gmail.com>
2026-04-20 02:42:37 +0000

    clean susfs
    Signed-off-by: SMGReborn <vherawidatama@gmail.com>

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -22,7 +22,8 @@
 #include "internal.h"
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-extern bool susfs_hide_sus_mnts_for_all_procs;
+extern bool susfs_hide_sus_mnts_for_non_su_procs;
+extern bool susfs_is_current_ksu_domain(void);
 #endif
 
 static __poll_t mounts_poll(struct file *file, poll_table *wait)
@@ -111,7 +112,10 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (susfs_hide_sus_mnts_for_all_procs && r->mnt_id >= DEFAULT_KSU_MNT_ID) {
+	if (susfs_hide_sus_mnts_for_non_su_procs &&
+		r->mnt_id >= DEFAULT_KSU_MNT_ID &&
+		!susfs_is_current_ksu_domain())
+	{
 		return 0;
 	}
 #endif
@@ -151,10 +155,14 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (susfs_hide_sus_mnts_for_all_procs && r->mnt_id >= DEFAULT_KSU_MNT_ID) {
+	if (susfs_hide_sus_mnts_for_non_su_procs &&
+		r->mnt_id >= DEFAULT_KSU_MNT_ID &&
+		!susfs_is_current_ksu_domain())
+	{
 		return 0;
 	}
 #endif
+
 	seq_printf(m, "%i %i %u:%u ", r->mnt_id, r->mnt_parent->mnt_id,
 		   MAJOR(sb->s_dev), MINOR(sb->s_dev));
 	if (sb->s_op->show_path) {
@@ -218,7 +226,10 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (susfs_hide_sus_mnts_for_all_procs && r->mnt_id >= DEFAULT_KSU_MNT_ID) {
+	if (susfs_hide_sus_mnts_for_non_su_procs &&
+		r->mnt_id >= DEFAULT_KSU_MNT_ID &&
+		!susfs_is_current_ksu_domain())
+	{
 		return 0;
 	}
 #endif
--- fs/stat.c
+++ fs/stat.c
@@ -162,6 +162,13 @@ EXPORT_SYMBOL(vfs_getattr);
  *
  * 0 will be returned on success, and a -ve error code if unsuccessful.
  */
+
+
+#ifdef CONFIG_KSU_SUSFS
+extern bool ksu_init_rc_hook __read_mostly;
+extern void ksu_handle_vfs_fstat(int fd, loff_t *kstat_size_ptr);
+#endif // #ifdef CONFIG_KSU_SUSFS
+
 int vfs_fstat(int fd, struct kstat *stat)
 {
 	struct fd f;
@@ -171,6 +178,11 @@ int vfs_fstat(int fd, struct kstat *stat)
 	if (!f.file)
 		return -EBADF;
 	error = vfs_getattr(&f.file->f_path, stat, STATX_BASIC_STATS, 0);
+#ifdef CONFIG_KSU_SUSFS
+	if (unlikely(ksu_init_rc_hook)) {
+		ksu_handle_vfs_fstat(fd, &stat->size);
+	}
+#endif // #ifdef CONFIG_KSU_SUSFS
 	fdput(f);
 	return error;
 }
