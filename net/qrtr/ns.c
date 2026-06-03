203aba2d1b9cc06519ace3e3ca94a499d82b041d
Maulana Riaddy <maulriaddy@gmail.com>
2026-06-03 08:10:38 +0000

    fixup! conflict merge
    Signed-off-by: Maulana Riaddy <maulriaddy@gmail.com>

--- net/qrtr/ns.c
+++ net/qrtr/ns.c
@@ -29,9 +29,9 @@ static struct {
 	struct socket *sock;
 	struct sockaddr_qrtr bcast_sq;
 	struct list_head lookups;
-	struct workqueue_struct *workqueue;
-	struct work_struct work;
-	void (*saved_data_ready)(struct sock *sk);
+	struct kthread_worker kworker;
+	struct kthread_work work;
+	struct task_struct *task;
 	int local_node;
 } qrtr_ns;
 
@@ -773,7 +773,6 @@ int qrtr_ns_init(void)
 		goto err_sock;
 	}
 
-	qrtr_ns.saved_data_ready = qrtr_ns.sock->sk->sk_data_ready;
 	qrtr_ns.sock->sk->sk_data_ready = qrtr_ns_data_ready;
 
 	sq.sq_port = QRTR_PORT_CTRL;
@@ -817,11 +816,7 @@ int qrtr_ns_init(void)
 	return 0;
 
 err_wq:
-	write_lock_bh(&qrtr_ns.sock->sk->sk_callback_lock);
-	qrtr_ns.sock->sk->sk_data_ready = qrtr_ns.saved_data_ready;
-	write_unlock_bh(&qrtr_ns.sock->sk->sk_callback_lock);
-
-	destroy_workqueue(qrtr_ns.workqueue);
+	kthread_stop(qrtr_ns.task);
 err_sock:
 	sock_release(qrtr_ns.sock);
 	return ret;
@@ -830,13 +825,8 @@ EXPORT_SYMBOL_GPL(qrtr_ns_init);
 
 void qrtr_ns_remove(void)
 {
-	write_lock_bh(&qrtr_ns.sock->sk->sk_callback_lock);
-	qrtr_ns.sock->sk->sk_data_ready = qrtr_ns.saved_data_ready;
-	write_unlock_bh(&qrtr_ns.sock->sk->sk_callback_lock);
-
-	cancel_work_sync(&qrtr_ns.work);
-	synchronize_net();
-	destroy_workqueue(qrtr_ns.workqueue);
+	kthread_flush_worker(&qrtr_ns.kworker);
+	kthread_stop(qrtr_ns.task);
 
 	/* sock_release() expects the two references that were put during
 	 * qrtr_ns_init(). This function is only called during module remove,
